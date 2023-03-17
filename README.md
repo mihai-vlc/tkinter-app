61c6bad2d9f6c2af447fb24d86870a7b651d5249
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2023-03-17 12:15:17 +0200

    Prints the background image

---
+++ README.md
@@ -0,0 +1,5 @@
+## References
+
+- https://tkdocs.com/
+- API reference: https://tkdocs.com/pyref/index.html
+- https://pillow.readthedocs.io/en/stable/reference/ImageTk.html
--- main.py
+++ main.py
@@ -1,9 +1,38 @@
-import tkinter as tk
+from tkinter import Canvas, Tk, TclVersion
+from PIL import Image, ImageTk
 
-root = tk.Tk()
+WIN_WIDTH = 600
+WIN_HEIGHT = 400
+
+root = Tk()
 root.title("My awesome application")
-root.configure()
+root.configure(background="#444")
+root.minsize(WIN_WIDTH, WIN_HEIGHT)
+root.geometry("+3000+300")
 
-if __name__ == '__main__':
+img_file = Image.open("./resources/background.jpg")
+background_canvas = Canvas(root, background="#555")
+background_canvas.pack(fill="both", expand=True)
+background_item = None
+
+resized_image = img_file.resize((WIN_WIDTH, WIN_HEIGHT))
+background_image = ImageTk.PhotoImage(resized_image)
+background_item = background_canvas.create_image(
+    0, 0, anchor="nw", image=background_image
+)
+
+
+def handle_resize(event):
+    resized_image = img_file.resize(size=(event.width, event.height))
+    background_image = ImageTk.PhotoImage(resized_image)
+    background_image.image = background_image
+    background_canvas.itemconfig(background_item, image=background_image)
+
+
+root.bind("<Configure>", handle_resize)
+
+print(f"running on {TclVersion = }")
+
+
+if __name__ == "__main__":
     root.mainloop()
-
